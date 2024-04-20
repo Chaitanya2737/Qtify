9f5514304c31f622e44ba7725bee7ab0a3e69ae9
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-04-20 20:55:57 +0530

    hero section testcase

--- src/component/Hero Section/Hero.js
+++ src/component/Hero Section/Hero.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import "./hero.css"
 import  HEADPHONE  from "../../assets/vibrating-headphone.svg"
+import "./hero.css"
 
 const Hero = () => {
   return (
-    <>  
-    <section className="Hero">
-        <div className="HeroTitle">
-            <h1>100 Thousand Songs, ad-free</h1>
-            <h1>Over thousands podcast episodes</h1>
-        </div>
-            <img className="HeroImage" src={HEADPHONE} alt='headphone'/>
-    </section>
-</>
+    <div className='wrapper'>
+      <div className='text'>
+        <h2>
+        100 Thousand Songs, ad-free
+        </h2>
+        <h2>
+        Over thousands podcast episodes
+        </h2>
+      </div>
+      <div className='heroImg'>
+        <img src={HEADPHONE} alt="" />
+      </div>
+    </div>
   )
 }
 
