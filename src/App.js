4b2394c0ba8d4919e9fe72aa2f8e738baef5b826
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-04-20 19:49:37 +0530

    hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './component/Navbar/Navbar'
+import Hero from './component/Hero Section/Hero'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
