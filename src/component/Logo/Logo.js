622fc7737d771e197edee9b626417ab9a4cc4b99
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-04-20 15:04:11 +0530

    Navbar testcase

--- src/component/Logo/Logo.js
+++ src/component/Logo/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <>
     <div className='position'>
-    <img style={{marginLeft:40}} src={LOGO} width={67} className='imgLogo' alt="Logo" />
+    <img  src={LOGO} width={67} className='imgLogo' alt="Logo" />
     </div>
     </>
 
